ce472825559faa939d59f588f6b7f81e0b0e0bc8
REDiowave <boschdoor.ian@gmail.com>
2019-11-14 13:41:07 -0800

    biz hours widget added

---
+++ plugins/biz-hours-widget/tent-biz-hours.php
@@ -0,0 +1,193 @@
+<?php
+
+/**
+ * RED WordPress Widget Boilerplate
+ *
+ * The RED Widget Boilerplate is an organized, maintainable boilerplate for building widgets using WordPress best practices.
+ *
+ * Lightly forked from the WordPress Widget Boilerplate by @tommcfarlin.
+ *
+ * @package   Tent Business Hours Widget
+ * @author    I.E. Doornbosch
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2015 I.E. Doornbosch
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Business Hours Widget
+ * Plugin URI:        @TODO
+ * Description:       Business hours widget for inhabitent
+ * Version:           1.0.0
+ * Author:            I.E.D.
+ * Author URI:        @TODO
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// Prevent direct file access
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+
+class Tent_Biz_Hours extends WP_Widget
+{
+
+	/**
+	 *
+	 * Unique identifier for your widget.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      string
+	 */
+	protected $widget_slug = 'tent-biz-hours';
+
+	/*--------------------------------------------------*/
+	/* Constructor
+	/*--------------------------------------------------*/
+
+	/**
+	 * Specifies the classname and description, and instantiates the widget.
+	 */
+	public function __construct()
+	{
+
+
+		parent::__construct(
+			$this->get_widget_slug(),
+			'Tent Biz Hours',
+			array(
+				'classname'  => $this->get_widget_slug() . '-class',
+				'description' => 'Business Hours Widget for Inhabitent custom theme'
+			)
+		);
+	} // end constructor
+
+	/**
+	 * Return the widget slug.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return    Plugin slug variable.
+	 */
+	public function get_widget_slug()
+	{
+		return $this->widget_slug;
+	}
+
+	/*--------------------------------------------------*/
+	/* Widget API Functions
+	/*--------------------------------------------------*/
+
+	/**
+	 * Outputs the content of the widget.
+	 *
+	 * @param array $args     The array of form elements
+	 * @param array $instance The current instance of the widget
+	 */
+	public function widget($args, $instance)
+	{
+
+		if (!isset($args['widget_id'])) {
+			$args['widget_id'] = $this->id;
+		}
+
+		// go on with your widget logic, put everything into a string and …
+
+		extract($args, EXTR_SKIP);
+
+		$widget_string = $before_widget;
+
+		// Manipulate the widget's values based on their input fields
+		$title = empty($instance['title']) ? '' : apply_filters('widget_title', $instance['title']);
+		$weekday = empty($instance['weekday']) ? '' : apply_filters('widget_weekday', $instance['weekday']);
+		$saturday = empty($instance['saturday']) ? '' : apply_filters('widget_saturday', $instance['saturday']);
+		$sunday = empty($instance['sunday']) ? '' : apply_filters('widget_sunday', $instance['sunday']);
+		// TODO: other fields go here...
+
+		ob_start();
+
+		if ($title) {
+			$widget_string .= $before_title;
+			$widget_string .= $title;
+			$widget_string .= $after_title;
+		}
+		if ($weekday) {
+			$widget_string .= $before_weekday;
+			$widget_string .= $weekday;
+			$widget_string .= $after_weekday;
+		}
+		if ($saturday) {
+			$widget_string .= $before_saturday;
+			$widget_string .= $saturday;
+			$widget_string .= $after_saturday;
+		}
+		if ($sunday) {
+			$widget_string .= $before_sunday;
+			$widget_string .= $sunday;
+			$widget_string .= $after_sunday;
+		}
+
+		include(plugin_dir_path(__FILE__) . 'views/widget.php');
+		$widget_string .= ob_get_clean();
+		$widget_string .= $after_widget;
+
+		print $widget_string;
+	} // end widget
+
+	/**
+	 * Processes the widget's options to be saved.
+	 *
+	 * @param array $new_instance The new instance of values to be generated via the update.
+	 * @param array $old_instance The previous instance of values before the update.
+	 */
+	public function update($new_instance, $old_instance)
+	{
+
+		$instance = $old_instance;
+
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['weekday'] = strip_tags($new_instance['weekday']);
+		$instance['saturday'] = strip_tags($new_instance['saturday']);
+		$instance['sunday'] = strip_tags($new_instance['sunday']);
+		// TODO: Here is where you update the rest of your widget's old values with the new, incoming values
+
+		return $instance;
+	} // end widget
+
+	/**
+	 * Generates the administration form for the widget.
+	 *
+	 * @param array $instance The array of keys and values for the widget.
+	 */
+	public function form($instance)
+	{
+
+		// TODO: Define default values for your variables, create empty value if no default
+		$instance = wp_parse_args(
+			(array) $instance,
+			array(
+				'title' => '',
+				'weekday' => '',
+				'saturday' => '',
+				'sunday' => '',
+			)
+		);
+
+		$title = strip_tags($instance['title']);
+		$weekday = strip_tags($instance['weekday']);
+		$saturday = strip_tags($instance['saturday']);
+		$sunday = strip_tags($instance['sunday']);
+		// TODO: Store the rest of values of the widget in their own variables
+
+		// Display the admin form
+		include(plugin_dir_path(__FILE__) . 'views/admin.php');
+	} // end form
+
+} // end class
+
+// TODO: Remember to change 'Widget_Name' to match the class name definition
+add_action('widgets_init', function () {
+	register_widget('Tent_Biz_Hours');
+});
--- plugins/biz-hours-widget/views/admin.php
+++ plugins/biz-hours-widget/views/admin.php
@@ -2,8 +2,19 @@
 
 <div class="widget-content">
    <p><label for="<?php echo $this->get_field_id('title'); ?>">Title:</label>
-		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>">
+      <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>">
    </p>
 
+   <p><label for="<?php echo $this->get_field_id('weekday'); ?>">Monday-Friday:</label>
+      <input class="widefat" id="<?php echo $this->get_field_id('weekday'); ?>" name="<?php echo $this->get_field_name('weekday'); ?>" type="text" value="<?php echo $weekday; ?>">
+   </p>
+
+   <p><label for="<?php echo $this->get_field_id('saturday'); ?>">Saturday:</label>
+      <input class="widefat" id="<?php echo $this->get_field_id('saturday'); ?>" name="<?php echo $this->get_field_name('saturday'); ?>" type="text" value="<?php echo $saturday; ?>">
+   </p>
+
+   <p><label for="<?php echo $this->get_field_id('sunday'); ?>">Sunday:</label>
+   <input class="widefat" id="<?php echo $this->get_field_id('sunday'); ?>" name="<?php echo $this->get_field_name('sunday'); ?>" type="text" value="<?php echo $sunday; ?>">
+   </p>
 
 </div>
