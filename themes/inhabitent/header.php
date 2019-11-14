ed3390daebe3b8729f2ddd83410f2fbe959c8b55
REDiowave <boschdoor.ian@gmail.com>
2019-11-14 09:05:48 -0800

    more layout fixes

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -1,36 +1,49 @@
 <?php
+
 /**
  * The header for our theme.
  *
  * @package RED_Starter_Theme
  */
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
 	<?php wp_head(); ?>
-	</head>
+</head>
 
-	<body <?php body_class(); ?>>
-		<div id="page" class="hfeed site">
-			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
+<body <?php body_class(); ?>>
+	<div id="page" class="hfeed site">
+		<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html('Skip to content'); ?></a>
 
-			<header id="masthead" class="site-header" role="banner">
+		<header id="masthead" class="site-header" role="banner">
+			<div class="container">
 				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				</div><!-- .site-branding -->
+					<a href="<?php echo get_site_url(); ?>">
+						<img class="header-logo" src="wp-content/themes/inhabitent/img/logos/inhabitent-logo-tent-white.svg" alt="Inhabitent">
+					</a>
+					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+				</div>
+			</div>
+			<!-- .site-branding -->
+			<nav id="site-navigation" class="main-navigation" role="navigation">
 
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
-			</header><!-- #masthead -->
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html('Primary Menu'); ?></button>
+				<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
 
-			<?php the_post_thumbnail(); ?>
+		<div class="front-hero">
+			<div class="front-hero-img"><?php the_post_thumbnail(); ?></div>
+			<!-- get_template_directory_uri(); -->
+				<span class="full-logo"><img src="wp-content/themes/inhabitent/img/logos/inhabitent-logo-full.svg" alt="Inhabitent"></span>
+		</div>
 
-			<div id="content" class="site-content">
+		<div id="content" class="site-content">
