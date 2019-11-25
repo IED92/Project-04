c563d6576b00af776e5b70beb8abf3b5c4757dba
REDiowave <boschdoor.ian@gmail.com>
2019-11-25 11:07:17 -0800

    styling changes

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -17,7 +17,7 @@ get_header(); ?>
 	</div>
 
 	<section class="product-info container">
-		<h2>Shop Stuff</h2>
+		<h2 class="shop-title">Shop Stuff</h2>
 		<?php
 		$terms = get_terms(array(
 			'taxonomy' => 'product_type',
@@ -27,10 +27,10 @@ get_header(); ?>
 			?>
 			<div class="product-type-blocks">
 				<?php foreach ($terms as $term) : ?>
-					<div class="product-type-block-wrapper">
-						<img src="<?php echo get_template_directory_uri() . '/images/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
+					<div class="product-type-wrapper">
+						<img src="<?php echo get_template_directory_uri() . '/img/product-type-icons/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
 						<p><?php echo $term->description; ?></p>
-						<p><a href="<?php echo get_term_link($term); ?>" class="btn"><?php echo $term->name; ?> Stuff</a></p>
+						<p><a href="<?php echo get_term_link($term); ?>" class="product-type-btn"><?php echo $term->name; ?> Stuff</a></p>
 					</div>
 				<?php endforeach; ?>
 			</div>
