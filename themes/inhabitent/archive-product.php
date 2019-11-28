bc3c3ae18cf966ba2c6b072d14e48816cea86932
REDiowave <boschdoor.ian@gmail.com>
2019-11-28 00:20:20 -0800

    shop wip

---
+++ themes/inhabitent/archive-product.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * The template for displaying archive of products.
+ *
+ * @package Inhabitent_Starter_Theme
+ */
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php if (have_posts()) : ?>
+
+			<header class="page-header">
+				<?php the_archive_title('<h1 class="page-title">', '</h1>') //the_archive_title( '<h1 class="page-title">', '</h1>' ); 
+					?>
+				<?php
+					$args = array('post_type' => 'product-type');
+					$terms = get_terms($args);
+					?>
+
+				<div class="product-stuff-links">
+					<?php foreach ($terms as $term) :; ?>
+						<a href="<?php echo get_site_url(); ?>/<?php echo $term->taxonomy; ?>/<?php echo $term->slug; ?>"><?php echo $term->name; ?></a>
+					<?php endforeach; ?>
+				</div>
+			</header><!-- .page-header -->
+
+			<?php /* Start the Loop */ ?>
+
+			<div class="product-archive-grid">
+				<?php while (have_posts()) : the_post(); ?>
+					<div class="product-grid-item">
+						<?php
+								get_template_part('template-parts/product', 'archive');
+								?>
+					</div>
+
+				<?php endwhile; ?>
+			</div>
+		<?php else : ?>
+
+			<?php get_template_part('template-parts/content', 'none'); ?>
+
+		<?php endif; ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ themes/inhabitent/template-parts/product-archive.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Template part for displaying product archives.
+ *
+ * @package RED_Starter_Theme
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <header class="entry-header">
+    <?php if (has_post_thumbnail()) : ?>
+      <div class="product-thumbnail-wrapper">
+        <a href="<?php echo get_post_permalink() ?>"><?php the_post_thumbnail('large'); ?></a>
+      </div>
+    <?php endif; ?>
+    <!-- <div class="entry-header__container"> -->
+    <div class="product-info">
+      <h2 class="product-title"><?php the_title(); ?></h2>
+      <span class="product-price"><?php get_field('product_price'); ?></span>
+      <span class="product-info-separator">...........................................................................................</span>
+    </div>
+  </header><!-- .entry-header -->
+</article><!-- #post-## -->
